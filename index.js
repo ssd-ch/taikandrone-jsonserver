0ea16551d8d9c5f8cd16de294d4d707684d963b7
ssd <ssd_chan@yahoo.co.jp>
2018-10-22 04:12:58 +0900

    create express server

---
+++ index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 3000));
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(request, response) {
+  response.send('Getではうまくサーバーが起動してます! 多分Postもできます')
+});
+
+app.post('/', function(req, res, next) {
+  res.json({
+    "fulfillmentText": "転スラが見つかりました"
+  });
+});
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+});
