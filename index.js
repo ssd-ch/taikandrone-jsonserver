647de7dd9462ad492f00081b75a0624855dd5d85
taku-an <33616291+taku-an@users.noreply.github.com>
2018-11-05 16:12:50 +0900

    elete body

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.get('/', function(request, response) {
 });
 
 app.post('/', function(req, res, next) {
-  var parameters = req.body.result.parameters;
+  var parameters = req.result.parameters;
   console.log(parameters);
   res.json({
     "fulfillmentText": "転スラ"
