953e1e705845dadef6b63c69fe7e04841bfcd3d5
taku-an <33616291+taku-an@users.noreply.github.com>
2018-11-07 19:56:56 +0900

    post beebotte

--- index.js
+++ index.js
@@ -1,28 +1,38 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var bbt = require('beebotte');
+var mqtt = require('mqtt');
 var app = express();
 
 app.set('port', (process.env.PORT || 3000));
 app.use(express.static(__dirname + '/public'));
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+var bclient = new bbt.Connector({apiKey: 'OFNz5cAYo7N8LJadi8B9mUM2',
+                                secretKey: 'jrtvfzHPIGuGa56U6R3GAz8WZwyRnFyO'});
+
 app.get('/', function(request, response) {
-  response.send('Getではうまくサーバーが起動してます! 多分Postもできます')
+  response.send('Getではうまくサーバーが起動してます! 多分Postもできます');
 });
 
 app.post('/', function(req, res, next) {
   res.setHeader('Content-Type', 'application/json');
-  var parameters = req.body.queryResult.parameters.drone_command;
+  // var parameters = req.body.queryResult.parameters.drone_command;
+  var parameters = req.body; //local version
   console.log(parameters);
-  // console.log("req = ", req);
-  // console.log("res = ", res);
-  // console.log("next = ", next);
+
+  bclient.publish(
+  {channel: 'MySmartHome', resource: 'voice', data: parameters},
+  function(err, res) {
+    // Do something here
+  });
+
   res.json({
-    "fulfillmentText": "転スラ"
+    'fulfillmentText': '転スラ'
   });
 });
 
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
+  console.log('Node app is running at localhost:' + app.get('port'));
 });
